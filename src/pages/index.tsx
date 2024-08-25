7492bc274c1dd973f038b3d42a2b0b19577a25f0
Gurpreet Bhopal <gbhopal91@gmail.com>
2024-08-25 15:19:36 +0100

    First commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import {builder, BuilderComponent} from "@builder.io/react";
+import "@/builder-registry"
+import {BuilderContent} from "@builder.io/sdk";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+export async function getStaticProps() {
+  const page = await builder
+      .get("page", {
+        userAttributes: {
+          urlPath: "/",
+        },
+      })
+      .toPromise();
+
+  return {
+    props: {
+      homepage: page || null,
+    },
+  };
+}
+
+export default function Home({homepage}: {homepage: BuilderContent | null}) {
+
+  console.log(homepage);
+  return        <BuilderComponent model="page" content={homepage || undefined} />
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
