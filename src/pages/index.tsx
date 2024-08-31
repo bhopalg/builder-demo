3c538ee8ab55199c435f2b61af909675d30cd8d0
Gurpreet Bhopal <gbhopal91@gmail.com>
2024-08-31 16:07:13 +0100

    Testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const page = await builder
       .get("page", {
         userAttributes: {
